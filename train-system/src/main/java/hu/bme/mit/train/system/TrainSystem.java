74945e95a5b58c86bcf52d2924dbddfe70d04249
Paróczi Gergő <gergo.paroczi@gmail.com>
2017-03-06 08:12:41 -0800

    implement tachograph

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -1,5 +1,7 @@
 package hu.bme.mit.train.system;
 
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 import hu.bme.mit.train.controller.TrainControllerImpl;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
@@ -7,11 +9,23 @@ import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
 
+import java.util.Date;
+
 public class TrainSystem {
 
+	private Table<String, String, String> tachographTable = HashBasedTable.create();
 	private TrainController controller = new TrainControllerImpl();
 	private TrainSensor sensor = new TrainSensorImpl(controller);
 	private TrainUser user = new TrainUserImpl(controller);
+
+	public TrainSystem() {
+
+		tachographTable.put("Date","101", new Date().toString());
+		tachographTable.put("JoystickPosition", "102",
+                Integer.toString(user.getJoystickPosition()));
+        tachographTable.put("ReferenceSpeed","103",
+                Integer.toString(controller.getReferenceSpeed()));
+	}
 
 	public TrainController getController() {
 		return controller;
